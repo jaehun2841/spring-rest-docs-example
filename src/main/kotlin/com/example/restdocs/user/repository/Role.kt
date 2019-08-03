eaac4f55a3b1c51c39e36f161f10c0577456c314
jaehun.lee <jaehun.lee@woowahan.com>
2019-08-03 23:52:07 +0900

    Revert "build spring rest docs environments"
    This reverts commit c89e4cc3f43a3e495e2a0446be80aeee2cea2ca5.

--- src/main/kotlin/com/example/restdocs/user/repository/Role.kt
+++
@@ -1,6 +0,0 @@
-package com.example.restdocs.user.repository
-
-data class Role(
-        val id: Long,
-        val name: String
-)
--- src/main/kotlin/com/example/restdocs/user/repository/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.restdocs.user.repository
-
-data class User(
-        val id: Long? = null,
-        val name: String,
-        val age: Int,
-        val address: String,
-        var roles: MutableList<Role> = mutableListOf()
-)
